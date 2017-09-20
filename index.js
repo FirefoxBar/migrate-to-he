6858d20728f4094ee20ed4f72aff0f815e5ae205
ShuangYa <896640547@qq.com>
2017-09-20 15:46:04 +0800

    Add redirector.uc.js support

--- index.js
+++ index.js
@@ -17,29 +17,22 @@ document.addEventListener("DOMContentLoaded", function() {
 		hm.src = "https://hm.baidu.com/hm.js?eddab75c23e1853a476011bb95a585c9";
 		document.head.appendChild(hm);
 	}
-
-	var fileElm = document.getElementById('file');
-	// Load a db from a file
-	fileElm.onchange = function() {
-		var f = fileElm.files[0];
-		var r = new FileReader();
-		r.onload = function() {
-			convert(r.result);
-		}
-		r.readAsText(f);
+	function initFile(fileEl, readCallback) {
+		fileEl.onchange = function() {
+			var f = this.files[0];
+			var r = new FileReader();
+			r.onload = function() {
+				readCallback(r.result);
+			}
+			r.readAsText(f);
+		};
 	}
-	
-	var rFileElm = document.getElementById('file-2');
-	rFileElm.onchange = function() {
-		var f = rFileElm.files[0];
-		var r = new FileReader();
-		r.onload = function() {
-			convertURL(r.result);
-		}
-		r.readAsText(f);
-	}
+	initFile(document.getElementById('file-1'), convert1);
+	initFile(document.getElementById('file-2'), convert2);
+	initFile(document.getElementById('file-3'), convert3);
 
-	function convert(rs) {
+	//Redirector
+	function convert1(rs) {
 		let n = JSON.parse(rs);
 		let r = [];
 		for (let item of n.redirects) {
@@ -59,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function() {
 				newItem.pattern = item.includePattern;
 				newItem.exclude = item.excludePattern;
 			}
-			if (item.processMatches === 'noProcessing') {
+			if (item.processMatches === 'noProcessing' || item.processMatches === 'urlDecode') {
 				newItem.to = item.redirectUrl;
 			} else {
 				newItem.isFunction = 1;
@@ -72,8 +65,6 @@ document.addEventListener("DOMContentLoaded", function() {
 				let functionName = '';
 				if (item.processMatches === 'urlEncode') {
 					functionName = 'encodeURIComponent';
-				} else if (item.processMatches === 'urlDecode') {
-					functionName = 'decodeURIComponent';
 				} else {
 					continue;
 				}
@@ -87,9 +78,10 @@ document.addEventListener("DOMContentLoaded", function() {
 			}
 			r.push(newItem);
 		}
-		download('headereditor.json', JSON.stringify({"request": r,"sendHeader": [],"receiveHeader": []}));
-	};
-	function convertURL(rs) {
+		download('headereditor.json', JSON.stringify({"request": r,"sendHeader": [],"receiveHeader": []}), 1);
+	}
+	//URLRedirector
+	function convert2(rs) {
 		let n = JSON.parse(rs);
 		let r = [];
 		for (let item of n.rules) {
@@ -105,18 +97,58 @@ document.addEventListener("DOMContentLoaded", function() {
 				"action": "redirect"
 			});
 		}
-		download('headereditor.json', JSON.stringify({"request": r,"sendHeader": [],"receiveHeader": []}), true);
-	};
+		download('headereditor.json', JSON.stringify({"request": r,"sendHeader": [],"receiveHeader": []}), 2);
+	}
+	//Redirect.uc.js
+	function convert3(rs) {
+		rs = rs.substr(rs.indexOf('[')).trim();
+		while (rs[rs.length - 1] !== ']') {
+			rs = rs.substr(0, rs.length - 1);
+		}
+		let n = null;
+		try {
+			n = eval('(' + rs + ')');
+		} catch (e) {
+			alert('Error');
+		}
+		let r = [];
+		for (let item of n) {
+			let newItem = {
+				"name": item.name,
+				"ruleType": "redirect",
+				"matchType": "",
+				"pattern": "",
+				"isFunction": 0,
+				"enable": 1,
+				"action": "redirect"
+			};
+			if (item.wildcard) {
+				newItem.matchType = "regexp";
+				newItem.pattern = item.from.replace(/\*/g, '(.*?)');
+				newItem.exclude = item.exclude ? item.exclude.replace(/\*/g, '(.*?)') : '';
+			} else if (item.regex) {
+				newItem.matchType = "regexp";
+				newItem.pattern = item.from.source;
+				newItem.exclude = item.exclude ? item.exclude.source : '';
+			} else {
+				newItem.matchType = "url";
+				newItem.pattern = item.from;
+				newItem.exclude = item.exclude ? item.exclude : '';
+			}
+			r.push(newItem);
+		}
+		download('headereditor.json', JSON.stringify({"request": r,"sendHeader": [],"receiveHeader": []}), 3);
+	}
 
-	function download(name, content, isSecond) {
-		var a = document.getElementById(isSecond ? 'download-2' : 'download');
+	function download(name, content, elementId) {
+		var a = document.getElementById('download-' + elementId);
 		var blob = new Blob([content]);
 		var evt = document.createEvent("MouseEvents");
 		evt.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
 		evt.initEvent("click", false, false);
 		a.download = name;
 		a.href = URL.createObjectURL(blob);
-		document.getElementById(isSecond ? 'download-label-2' : 'download-label').style.display = "block";
+		document.getElementById('download-label-' + elementId).style.display = "block";
 		a.dispatchEvent(evt);
 	};
 });
